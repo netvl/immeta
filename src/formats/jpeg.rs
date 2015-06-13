407f350840733328bc25635774a766afc45457a8
Vladimir Matveev <vladimir.matweev@gmail.com>
2015-06-13 18:17:46 +0300

    Improved jpeg metadata parser

--- src/formats/jpeg.rs
+++ src/formats/jpeg.rs
@@ -27,12 +27,11 @@ impl LoadableMetadata for Metadata {
     fn load<R: ?Sized + Read>(r: &mut R) -> Result<Metadata> {
         let mut r = &mut BufReader::new(r);
         loop {
-            let bytes_read = try!(r.skip_until(0xff));
-            if bytes_read == 0 {
+            if try!(r.skip_until(0xff)) == 0 {
                 return Err(unexpected_eof!("when searching for a marker"));
             }
             
-            let marker_type = try!(r.read_u8().map_err(if_eof!("when reading marker type")));
+            let marker_type = try_if_eof!(r.read_u8(), "when reading marker type");
             if marker_type == 0 { continue; }  // skip "stuffed" byte
 
             let has_size = match marker_type {
@@ -41,16 +40,15 @@ impl LoadableMetadata for Metadata {
             };
 
             let size = if has_size {
-                try!(r.read_u16::<BigEndian>()
-                    .map_err(if_eof!("when reading marker payload size"))) - 2
+                try_if_eof!(r.read_u16::<BigEndian>(), "when reading marker payload size") - 2
             } else { 0 };
 
             let dimensions = match marker_type {
                 0xc0 | 0xc2 => {  // maybe others?
                     // skip one byte
-                    try!(r.read_u8().map_err(if_eof!("when skipping to dimensions data")));
-                    let h = try!(r.read_u16::<BigEndian>().map_err(if_eof!("when reading height")));
-                    let w = try!(r.read_u16::<BigEndian>().map_err(if_eof!("when reading width")));
+                    let _ = try_if_eof!(r.read_u8(), "when skipping to dimensions data");
+                    let h = try_if_eof!(r.read_u16::<BigEndian>(), "when reading height");
+                    let w = try_if_eof!(r.read_u16::<BigEndian>(), "when reading width");
                     Some((w, h))
                 }
                 _ => None
@@ -69,6 +67,4 @@ impl LoadableMetadata for Metadata {
         }
     }
 }
-
-
 
